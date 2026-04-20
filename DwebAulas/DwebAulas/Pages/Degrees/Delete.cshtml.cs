67a124c2c1b3eabd01bac35b94325c77278deef6
Filipe Branco <estt6967@ipt.pt>
2026-04-20 09:47:21 +0100

    Classes para a interacção com degree

---
+++ DwebAulas/DwebAulas/Pages/Degrees/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Aulas.Data.Model;
+using DwebAulas.Data;
+
+namespace DwebAulas.Pages.Degrees
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DwebAulas.Data.ApplicationDbContext _context;
+
+        public DeleteModel(DwebAulas.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Degree Degree { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degree = await _context.Degrees.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (degree is not null)
+            {
+                Degree = degree;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degree = await _context.Degrees.FindAsync(id);
+            if (degree != null)
+            {
+                Degree = degree;
+                _context.Degrees.Remove(Degree);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ DwebAulas/DwebAulas/Pages/Degrees/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Aulas.Data.Model;
+using DwebAulas.Data;
+
+namespace DwebAulas.Pages.Degrees
+{
+    public class EditModel : PageModel
+    {
+        private readonly DwebAulas.Data.ApplicationDbContext _context;
+
+        public EditModel(DwebAulas.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Degree Degree { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var degree =  await _context.Degrees.FirstOrDefaultAsync(m => m.Id == id);
+            if (degree == null)
+            {
+                return NotFound();
+            }
+            Degree = degree;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Degree).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DegreeExists(Degree.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DegreeExists(int id)
+        {
+            return _context.Degrees.Any(e => e.Id == id);
+        }
+    }
+}
